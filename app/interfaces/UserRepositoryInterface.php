63c65d04b50397d88f9e083663e448d3f469e0ca
AAYOUBhou44a <ayoubhoudda385@gmail.com>
2026-01-21 10:28:58 +0100

    La conception

---
+++ app/interfaces/UserRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+interface UserRepositoryInterface{
+    public function addClass();
+    // public function getProgressAll();
+    public function addSprint();
+    public function addSkill();
+    public function addMember();
+    public function addComment();
+    public function evaluateStudent();
+    public function addBrief();
+    public function delivreBrief();
+}
+
+?>
